9f3a2310a36783f2dd0389b082e7973fc49cf850
Holt Walborn <hwalborn@gmail.com>
2017-01-16 20:28:32 +0000

    Dirty and Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  difference_index = find_first_difference(arr)
+  arr.sort_by! {|phrase|
+    esper_index(phrase, difference_index)
+  }
+end
+
+def esper_index(phrase, difference)
+  esperanto_alphabet= "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  esperanto_alphabet.each_with_index {|letter, index|
+    if phrase[difference] == letter
+      return index
+    end
+  }
+end
+
+def find_first_difference(arr)
+  index = 0
+  while arr[0][index] == arr[1][index]
+    index += 1
+  end
+  index
 end
